030229ba4235be09a1cbf992a81eea1ce40574b0
ivarsrb <ivars.rb@gmail.com>
2019-09-26 09:43:09 +0300

    Add vertex buffer to terrain

--- src/renderable/terrain.cpp
+++ src/renderable/terrain.cpp
@@ -6,14 +6,18 @@ Terrain::Terrain(renderer::Renderer& renderer) :
     height_grid_(GenerateHeightGrid(5)),
     renderer_(renderer),
     pipeline_id_(renderer_.GetPipelineManager().AddGraphicsPipeline(GetPipelineDescription(),
-        renderer_.GetWindow().GetRenderPass(), renderer_.GetWindow().GetSwapchainObject().GetExtent())) {
+        renderer_.GetWindow().GetRenderPass(), renderer_.GetWindow().GetSwapchainObject().GetExtent())),
+    vertices_(GetVertices()),
+    vertex_buffer_("terrain vertices", renderer_.GetMemoryAllocator(),
+        (renderer::vlk::BufferSize)(vertices_.size() * sizeof(vertices_[0])), vertices_.data()) {
     base::Log::Info("Renderable: terrain created");
 }
 
 // Add command to given command buffer that is already in recording state
 void Terrain::AppendCommandBuffer(const renderer::vlk::CommandBuffer& command_buffer) const {
     command_buffer.BindGraphicsPipeline(renderer_.GetPipelineManager().GetGraphicsPipeline(pipeline_id_));
-    command_buffer.Draw(3, 1, 0, 0);
+    command_buffer.BindVertexBuffer(vertex_buffer_.Get());
+    command_buffer.Draw(static_cast<uint32_t>(vertices_.size()), 1, 0, 0);
 }
 
 renderer::vlk::GraphicsPipeline::CreateParams Terrain::GetPipelineDescription() {
@@ -22,15 +26,15 @@ renderer::vlk::GraphicsPipeline::CreateParams Terrain::GetPipelineDescription() 
         "Terrain pipeline",
         // Programmable stage
         {
-        // Shader stages
-        {"shaders/vert.spv", renderer::vlk::GraphicsPipeline::ShaderStage::kVertex },
-        {"shaders/frag.spv", renderer::vlk::GraphicsPipeline::ShaderStage::kFragment },
-    },
-    // Vertex input params
-    {
-        // Vertex data is hardcoded into vertex shader
-        {},{}
-    },
+            // Shader stages
+            {"shaders/vert_vb.spv", renderer::vlk::GraphicsPipeline::ShaderStage::kVertex },
+            {"shaders/frag_vb.spv", renderer::vlk::GraphicsPipeline::ShaderStage::kFragment },
+        },
+        // Vertex input params
+        {
+            renderer::VertexPos2dColor::GetBindingDescription(),
+            renderer::VertexPos2dColor::GetAttributeDescriptions(),
+        },
         // Fixed function state
         // Primitive assembly
         {
@@ -64,7 +68,16 @@ base::Matrix<t::F32> Terrain::GenerateHeightGrid(t::U16 size) const {
     base::Matrix<t::F32> height_grid(size, size);
     height_grid(0, 0) = 1.0;
     height_grid(size - 1, size - 1) = 2.0;
-
     return height_grid;
+}
+
+// Generate vertices from height grid
+std::vector<renderer::VertexPos2dColor> Terrain::GetVertices() const {
+    std::vector<renderer::VertexPos2dColor> vertices = {
+        {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+        {{0.5f, 0.5f}, {0.0f, 0.5f, 0.0f}},
+        {{-0.5f, 0.5f}, {1.0f, 0.0f, 1.0f}}
+        };
+    return vertices;
 }
 }; // renderable
--- src/renderable/terrain.h
+++ src/renderable/terrain.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <vector>
 #include "i_renderable.h"
+#include <renderer/types.h>
 #include <renderer/renderer.h>
+#include <renderer/vlk/vertex_buffer.h>
 #include <base/matrix.h>
 
 // Terrain representation as a renderable object
@@ -14,6 +17,7 @@ private:
     // Generate and return height grid populated with height values that define
     // terrain height.
     base::Matrix<t::F32> GenerateHeightGrid(t::U16 size) const;
+    std::vector<renderer::VertexPos2dColor> GetVertices() const;
     // Height grid representation as 2d matrix
     const base::Matrix<t::F32> height_grid_;
     // Reference to renderer this triangle is tied with
@@ -22,5 +26,7 @@ private:
     // Can't store pipeline handle directly, because pipelines are recreated upon screen
     // resize. But id is tied to specific location in pipeline catche
     const renderer::PipelineId pipeline_id_;
+    const std::vector<renderer::VertexPos2dColor> vertices_;
+    const renderer::vlk::VertexBuffer vertex_buffer_;
 };
 }; // renderable
