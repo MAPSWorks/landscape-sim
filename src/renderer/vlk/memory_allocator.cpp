de6834d4b5bc24ee637797bb4138e1fac16bfb9f
ivarsrb <ivars.rb@gmail.com>
2019-10-24 12:36:44 +0300

    Clean memory allocator module

--- src/renderer/vlk/memory_allocator.cpp
+++ src/renderer/vlk/memory_allocator.cpp
@@ -4,10 +4,7 @@
 
 namespace renderer::vlk {
 MemoryAllocator::MemoryAllocator(const Device& device):
-    device_(device),
-    allocator_(Create(device_)){
-    // if graphics is supported, transfer also implicitly is supported
-   // transfer_command_pool_(device_.Get(), device_.GetQueue().GetFamilyIndices().graphics.value(), false, true) {
+    allocator_(Create(device)){
     base::Log::Info("Renderer: GPU memory allocator created");
 }
 
@@ -42,19 +39,4 @@ VmaAllocator MemoryAllocator::Create(const Device& device) const {
     ErrorCheck(vmaCreateAllocator(&allocator_info, &allocator));
     return allocator;
 }
-/*
-// Source buffer must be host visible and transfer source, destination buffer must be transfer destination
-void MemoryAllocator::CopyBuffer(const VkBuffer& src_buffer, const VkBuffer& dst_buffer, BufferSize size) const {
-    CommandBuffer command_buffer(transfer_command_pool_);
-    command_buffer.Begin(CommandBuffer::Usage::kOneTimeSubmit);
-    command_buffer.CopyBuffer(src_buffer, dst_buffer, size);
-    command_buffer.End();
-    // Submit to graphics queue because graphics queue implicitly supports transfer
-    device_.GetQueue().Submit(device_.GetQueue().GetGraphics(), command_buffer.Get());
-    // Since we dont use fence
-    ErrorCheck(vkQueueWaitIdle(device_.GetQueue().GetGraphics()));
-    // Freeing is not ind command buffers destructor because it is not mandatory
-    command_buffer.Free(device_.Get());
-    base::Log::Info("Renderer: buffer copy performed. Size - ", size);
-}*/
 }; // renderer::vlk
--- src/renderer/vlk/memory_allocator.h
+++ src/renderer/vlk/memory_allocator.h
@@ -3,7 +3,6 @@
 #include <allocator/vk_mem_alloc.h>
 #include "vulkan_shared.h"
 #include "device.h"
-//#include "command_pool.h"
 
 // Wrapper around AMD vulkan memory allocatr application wide renderable that manages
 // buffer memory allocations.
@@ -18,18 +17,9 @@ public:
     const VmaAllocator& Get() const;
     // Retrieve and print statistics of this allocator
     void DebugPrint() const;
-    // Helper
-    // Copies contents of one buffer to other
-    //void CopyBuffer(const VkBuffer& src_buffer, const VkBuffer& dst_buffer, BufferSize size) const;
 private:
     VmaAllocator Create(const Device& device) const;
-    // Reference to resource this is created with
-    const Device& device_;
     // AMD memory allocator
     const VmaAllocator allocator_;
-    // Helper
-    // Command pool for short-lived command buffers for memory transfers from buffer to buffer.
-    // Driver should be able to optimaze if we use separate pool for these transient operation.
-    //const CommandPool transfer_command_pool_;
 };
 }; // renderer::vlk
