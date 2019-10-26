92c73273f04361bfab6b34cb443152ceb4f601f4
ivarsrb <ivars.rb@gmail.com>
2019-10-26 08:55:48 +0300

    Add update descriptor set with generic resources

--- src/renderer/vlk/descriptor_set.cpp
+++ src/renderer/vlk/descriptor_set.cpp
@@ -15,7 +15,7 @@ const VkDescriptorSet& DescriptorSet::Get() const {
 
 void DescriptorSet::UpdateUniformBuffer(const VkBuffer& buffer, t::U64 buffer_size) const {
     VkDescriptorBufferInfo buffer_info{};
-    buffer_info.buffer = buffer;
+    buffer_info.buffer = buffer; 
     buffer_info.offset = 0;
     // Update with given buffer size, except when passed 0 - use whole size
     buffer_size == 0 ? buffer_info.range = VK_WHOLE_SIZE : buffer_info.range = buffer_size;
@@ -30,6 +30,48 @@ void DescriptorSet::UpdateUniformBuffer(const VkBuffer& buffer, t::U64 buffer_si
     vkUpdateDescriptorSets(device_, 1, &descriptor_write, 0, nullptr);
     
     base::Log::Info("Renderer: descriptor set updated with buffer of size (", buffer_size, "). '0' means whole size");
+}
+
+void DescriptorSet::Update(const std::vector<ResourcesToUpdate>& resources_to_update) const {
+    std::vector<VkWriteDescriptorSet> descriptor_writes;
+    for (size_t i = 0; i < resources_to_update.size(); ++i) {
+        const auto& resource = resources_to_update.at(i);
+        VkWriteDescriptorSet descriptor_write {};
+        descriptor_write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptor_write.dstSet = descriptor_set_;
+        // TODO: maybe this index should be location in shader?
+        descriptor_write.dstBinding = static_cast<t::U32>(i);
+        // For uniform buffers
+        if (resource.type == DescriptorType::kUniformBuffer) {
+            VkDescriptorBufferInfo buffer_info {};
+            buffer_info.buffer = resource.buffer;
+            buffer_info.offset = resource.buffer_offset;
+            // Update with given buffer size, except when passed 0 - use whole size
+            resource.buffer_range == 0 ? buffer_info.range = VK_WHOLE_SIZE : buffer_info.range = resource.buffer_range;
+            descriptor_write.dstArrayElement = 0;
+            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+            descriptor_write.descriptorCount = resource.count;
+            descriptor_write.pBufferInfo = &buffer_info;
+            base::Log::Info("Renderer: descriptor set to be updated with uniform buffer resource of size -", buffer_info.range);
+        } else
+        // For combined image sampler
+        if (resource.type == DescriptorType::kCombinedImageSampler) {
+            VkDescriptorImageInfo image_info {};
+            image_info.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+            image_info.imageView = resource.image_view;
+            image_info.sampler = resource.image_sampler;
+            descriptor_write.dstArrayElement = 0;
+            descriptor_write.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+            descriptor_write.descriptorCount = resource.count;
+            descriptor_write.pImageInfo = &image_info;
+            base::Log::Info("Renderer: descriptor set to be updated with comb. image sampler resource");
+        }
+        else {
+            throw std::runtime_error("Renderer: unrecognized descriptor type tried to update descriptor set");
+        }
+        descriptor_writes.push_back(descriptor_write);
+    }
+    vkUpdateDescriptorSets(device_, static_cast<t::U32>(descriptor_writes.size()), descriptor_writes.data(), 0, nullptr);
 }
 
 VkDescriptorSet DescriptorSet::Allocate(const VkDescriptorPool& pool, const VkDescriptorSetLayout& layout) const {
--- src/renderer/vlk/descriptor_set.h
+++ src/renderer/vlk/descriptor_set.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <base/types.h>
 #include "vulkan_shared.h"
+#include "types.h"
 
 // Stores a descriptor set that is created from given descriptor pool.
 // Descriptor set is a collection of descriptors.
@@ -9,6 +10,22 @@
 namespace renderer::vlk {
 class DescriptorSet {
 public:
+    // Resource data that is passed to update (bind) to descriptor set
+    struct ResourcesToUpdate {
+        // Type of a descriptor within this set to update
+        DescriptorType type;
+        // How many descriptor of this type to update
+        t::U32 count;
+        // For buffers only
+        VkBuffer buffer;
+        // Offset for this buffer
+        BufferSize buffer_offset;
+        // Range to update (0 means whole)
+        BufferSize buffer_range;
+        // For image samplers only
+        VkImageView image_view;
+        VkSampler image_sampler;
+    };
     // pool - descriptor pool to allocate from.
     // layout - layouts ths set is going to be based on (descriptor set layout is kinda like type)
     DescriptorSet(const VkDevice& device, const VkDescriptorPool& pool, const VkDescriptorSetLayout& layout);
@@ -17,6 +34,8 @@ public:
     // Update descriptor set with given uniform buffer
     // If buffer size is passed 0, use whole buffer size to bind
     void UpdateUniformBuffer(const VkBuffer& buffer, t::U64 buffer_size = 0) const;
+    // Update this descriptor set with actual resources
+    void Update(const std::vector<ResourcesToUpdate>& resources_to_update) const;
 private:
     // Reference to resource this renderable is created with
     const VkDevice& device_;
