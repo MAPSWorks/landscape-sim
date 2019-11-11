eff52632747763be70429d7ed5a1a4d52e271ee6
ivarsrb <ivars.rb@gmail.com>
2019-11-11 12:12:39 +0200

    Add depth image module

---
+++ src/renderer/depth_image.cpp
@@ -0,0 +1,19 @@
+#include "depth_image.h"
+
+namespace renderer {
+DepthImage::DepthImage(const VkDevice& device, const vlk::MemoryAllocator& allocator, const VkExtent2D& extent) :
+    // Specifies a two - component, 32 - bit packed format that has 8 unsigned integer bits in the stencil component, 
+    // and 24 unsigned normalized bits in the depth component
+    format_(VK_FORMAT_D24_UNORM_S8_UINT),
+    image_("depth image", allocator, VK_IMAGE_TYPE_2D,
+        VkExtent3D{ extent.width, extent.height ,1 }, format_, VK_IMAGE_TILING_OPTIMAL,
+        VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT, VMA_MEMORY_USAGE_GPU_ONLY),
+    image_view_(device, image_.Get(), format_)
+{
+}
+
+const VkFormat& DepthImage::GetFormat() const {
+    return format_;
+}
+
+}; // renderer
---
+++ src/renderer/depth_image.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "vlk/image.h"
+#include "vlk/image_view.h"
+#include "vlk/memory_allocator.h"
+
+// Image to store pixel depth information.
+// Depth image resource, view and format properties.
+namespace renderer {
+class DepthImage {
+public:
+    DepthImage(const VkDevice& device, const vlk::MemoryAllocator& allocator, const VkExtent2D& extent);
+    const VkFormat& GetFormat() const;
+private:
+    // Depth image format
+    const VkFormat format_;
+    const vlk::Image image_;
+    const vlk::ImageView image_view_;
+};
+}; // renderer
