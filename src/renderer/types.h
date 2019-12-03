775e354d717023321cbe3b6f456dbfd69df4ae35
ivarsrb <ivars.rb@gmail.com>
2019-12-03 12:25:11 +0200

    Add vertex type of position 2d

--- src/renderer/types.h
+++ src/renderer/types.h
@@ -11,6 +11,36 @@ namespace renderer {
 constexpr t::U32 VertexShaderPositionLocation = 0;
 constexpr t::U32 VertexShaderColorLocation = 1;
 constexpr t::U32 VertexShaderTexLocation = 2;
+// Describe vertex attributes for 2d position
+struct VertexPos2d {
+    t::Vec2 position;
+    // Vertex binding defines a collection of data taken from a vertex buffer bound to a 
+    // selected index
+    // This binding is used as a numbered source of data for vertex attributes.
+    static std::vector<VkVertexInputBindingDescription> GetBindingDescription() {
+        std::vector<VkVertexInputBindingDescription> binding_descr(1);
+        // From which binding data is taken from
+        const t::U32 binding = 0;
+        binding_descr[0].binding = binding;
+        // Jump from vertex to vertex
+        binding_descr[0].stride = sizeof(VertexPos2d);
+        // Fetched per vertex
+        binding_descr[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return binding_descr;
+    }
+    // Describes how to get vertex attributes from a chunk of data described by vertex input binding
+    static std::vector<VkVertexInputAttributeDescription> GetAttributeDescriptions() {
+        std::vector<VkVertexInputAttributeDescription> attribute_descr(1);
+        // From which binding data comes from
+        const t::U32 binding = 0;
+        attribute_descr[0].binding = binding;
+        // location= in the Vertex shader
+        attribute_descr[0].location = VertexShaderPositionLocation;
+        attribute_descr[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attribute_descr[0].offset = offsetof(VertexPos2d, position);
+        return attribute_descr;
+    }
+};
 // Describe vertex attributes for 2d position and color
 struct VertexPos2dColor {
     t::Vec2 position;
