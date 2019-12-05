a192c6c2f73488922fb7a32a35ac66896660dce0
ivarsrb <ivars.rb@gmail.com>
2019-12-05 14:34:19 +0200

    Add meters per vulkan unit scale parameter

--- src/scene/types.h
+++ src/scene/types.h
@@ -2,6 +2,10 @@
 #include <base/types.h>
 
 namespace scene {
+// How many meters are meant by single Vulkan unit.
+// This field is integer to not mess up caluculation precision and it is unlikely
+// to be set under 1 or somewhere between two integers.
+static constexpr t::U32 kMetersPerUnit = 1;
 // Layout for shader resources
 // Set number of a given descriptor set in shader (set=n)
 enum DescruptorSetSlotId {
