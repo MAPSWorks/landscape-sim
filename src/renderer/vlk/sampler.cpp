7f0d8902404564eacd056b12e20a7a84fcdb2fc0
ivarsrb <ivars.rb@gmail.com>
2019-12-05 13:41:10 +0200

    Add different usage modes for sampler

--- src/renderer/vlk/sampler.cpp
+++ src/renderer/vlk/sampler.cpp
@@ -1,10 +1,11 @@
 #include "sampler.h"
+#include <stdexcept>
 #include <base/log.h>
 
 namespace renderer::vlk {
-Sampler::Sampler(const VkDevice& device) :
+Sampler::Sampler(const VkDevice& device, UsageMode usage_mode) :
     device_(device),
-    sampler_(Create()) {
+    sampler_(Create(usage_mode)) {
     base::Log::Info("Renderer: sampler created");
 }
 
@@ -17,21 +18,37 @@ const VkSampler& Sampler::Get() const {
     return sampler_;
 }
 
-VkSampler Sampler::Create() const {
+VkSampler Sampler::Create(UsageMode usage_mode) const {
     VkSamplerCreateInfo create_info {};
     create_info.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    // Common parameters equal for every usage mode
     create_info.magFilter = VK_FILTER_LINEAR;
     create_info.minFilter = VK_FILTER_LINEAR;
     create_info.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     create_info.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     create_info.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-    create_info.anisotropyEnable = VK_TRUE;
-    create_info.maxAnisotropy = 16;
     create_info.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-    create_info.unnormalizedCoordinates = VK_FALSE;
     create_info.compareEnable = VK_FALSE;
     create_info.compareOp = VK_COMPARE_OP_ALWAYS;
     create_info.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+    // Switch creation parameters dependant on usage mode
+    switch (usage_mode) {
+    // Most common usage of a sampler - simple texture
+    case UsageMode::kDefault:
+        create_info.anisotropyEnable = VK_TRUE;
+        create_info.maxAnisotropy = 16;
+        create_info.unnormalizedCoordinates = VK_FALSE;
+        break;
+    // Sampler used for height data reading in shader
+    case UsageMode::kHeightmap:
+        create_info.anisotropyEnable = VK_FALSE;
+        // Texel coordinates in shader will go from 0 - width/height of image
+        create_info.unnormalizedCoordinates = VK_TRUE;
+        break;
+    default:
+        throw std::runtime_error("Renderer: sampler usage mode unknown");
+        break;
+    }
     VkSampler sampler;
     ErrorCheck(vkCreateSampler(device_, &create_info, nullptr, &sampler));
     return sampler;
--- src/renderer/vlk/sampler.h
+++ src/renderer/vlk/sampler.h
@@ -5,13 +5,21 @@
 namespace renderer::vlk {
 class Sampler {
 public:
-    Sampler(const VkDevice& device);
+    // Common usage modes for given sampler.
+    // Given usage mode uses different sampler create prameter combination.
+    enum class UsageMode {
+        // The most common usage as a texture (default value)
+        kDefault,
+        // Used as a source for height data in shader
+        kHeightmap
+    };
+    Sampler(const VkDevice& device, UsageMode usage_mode = UsageMode::kDefault);
     ~Sampler();
     Sampler(Sampler const&) = delete;
     Sampler operator=(Sampler const&) = delete;
     const VkSampler& Get() const;
 private:
-    VkSampler Create() const;
+    VkSampler Create(UsageMode usage_mode) const;
     // Reference to resource this renderable is created with
     const VkDevice& device_;
     const VkSampler sampler_ = VK_NULL_HANDLE;
