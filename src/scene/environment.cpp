7e07cf5c0b1b1d82fd98c6e79a44f6868349fb4c
ivarsrb <ivars.rb@gmail.com>
2019-12-31 08:53:19 +0200

    Add gui to change sunlight direction

--- src/scene/environment.cpp
+++ src/scene/environment.cpp
@@ -1,4 +1,5 @@
 #include "environment.h"
+#include <glm/gtx/rotate_vector.hpp>
 #include <imgui/imgui.h>
 
 namespace scene {
@@ -10,9 +11,16 @@ const t::Vec3& Environment::GetSunlightDirection() const {
     return sunlight_direction_;
 }
 
-void Environment::UpdateGUI() const {
+void Environment::UpdateGUI()  const {
     ImGui::Begin("Environment");
-
+    ImGui::Text("Sunlight direction: (%0.2f, %0.2f, %0.2f)", sunlight_direction_.x, sunlight_direction_.y, sunlight_direction_.z);
+    static t::F32 vertical_angle = 0;
+    ImGui::SliderFloat("Vertical angle ", &vertical_angle, -0.5f, 3.14f / 2);
+    static t::F32 horizontal_angle = 0;
+    ImGui::SliderFloat("Horizontal angle ", &horizontal_angle, 0.0f, 2*3.14f);
+    // anges 0 and 0 give light direction of (0,0,1)
+    sunlight_direction_ = glm::rotateX(t::Vec3(0.0, 0.0, 1.0), vertical_angle);
+    sunlight_direction_ = glm::rotateY(sunlight_direction_, horizontal_angle);
     ImGui::End();
 }
 }; //  scene
--- src/scene/environment.h
+++ src/scene/environment.h
@@ -8,9 +8,9 @@ public:
     Environment();
     const t::Vec3& GetSunlightDirection() const;
     // Prepare user interface to modify environment values.
-    void UpdateGUI() const;
+    void UpdateGUI() const ;
 private:
     // Direction vector from sunlight to origin
-    t::Vec3 sunlight_direction_;
+    mutable t::Vec3 sunlight_direction_;
 };
 }; //  scene
