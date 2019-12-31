b04055ec0cbb0bd31edc90e76ffebf692e871887
ivarsrb <ivars.rb@gmail.com>
2019-12-31 12:59:40 +0200

    Remove unused shaders

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,9 +1,4 @@
 ::for /r %%i in (*.frag, *.vert) do %VULKAN_SDK%/Bin/glslangValidator.exe -V %%i
-C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\vert.spv -V shader.vert
-C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\frag.spv -V shader.frag
-
-C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\vert_vb.spv -V shader_vb.vert
-C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\frag_vb.spv -V shader_vb.frag
 
 C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\terrain_vert.spv -V terrain.vert
 C:\Libs\VulkanSDK\Bin32/glslangValidator.exe -o ..\product\shaders\terrain_frag.spv -V terrain.frag
