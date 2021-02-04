1caba0fdabc28789ac19ed154a55ba5dcbd0b6ae
ivarsrb <ivars.rb@gmail.com>
2021-02-04 19:17:10 +0200

    Add swapchain module

---
+++ include/lsim/renderer/vlk/swapchain.h
@@ -0,0 +1,11 @@
+//
+// Created by Ivars Rusbergs in 2021
+//
+#ifndef LSIM_RENDERER_VLK_SWAPCHAIN_H_
+#define LSIM_RENDERER_VLK_SWAPCHAIN_H_
+
+namespace lsim::renderer::vlk {
+
+}
+
+#endif
---
+++ src/renderer/vlk/swapchain.cc
@@ -0,0 +1,8 @@
+//
+// Created by Ivars Rusbergs in 2021
+//
+#include "lsim/renderer/vlk/swapchain.h"
+
+namespace lsim::renderer::vlk {
+
+}
