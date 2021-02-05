e9fc55ede7a80204b3a5323922338a82d908d9b6
ivarsrb <ivars.rb@gmail.com>
2021-02-05 09:51:09 +0200

    Disable linear math types

--- include/lsim/base/types.h
+++ include/lsim/base/types.h
@@ -6,7 +6,7 @@
 #define LSIM_TYPES_H_
 
 //#define GLM_FORCE_MESSAGES
-#include <glm/glm.hpp>
+//#include <glm/glm.hpp>
 
 namespace lsim {
 using F32 = float;
@@ -21,6 +21,7 @@ struct Size {
     T height;
 };
 // GLM linear math types aliasing
+/*
 using Vec2 = glm::tvec2<F32>;
 using Vec3 = glm::tvec3<F32>;
 using Vec4 = glm::tvec4<F32>;
@@ -31,6 +32,7 @@ using Vec2i32 = glm::tvec2<int32_t>;
 using Vec3i32 = glm::tvec3<int32_t>;
 using Mat3 = glm::tmat3x3<F32>;
 using Mat4 = glm::tmat4x4<F32>;
+*/
 // Type compile-time checks
 static_assert(sizeof(F32) == 4);
 static_assert(sizeof(F64) == 8);
