d57680dfe3b701f01b057c51c9169cef581787e0
ivarsrb <ivars.rb@gmail.com>
2021-02-09 08:56:57 +0200

    Format layout module

--- src/renderer/vlk/pipeline_layout.cc
+++ src/renderer/vlk/pipeline_layout.cc
@@ -9,25 +9,25 @@
 #include "vulkan_shared.h"
 
 namespace lsim::renderer::vlk {
-PipelineLayout::PipelineLayout(const VkDevice& device, const VkPipelineLayoutCreateInfo &create_info) :
-    device_(device),
-    pipeline_layout_(Create(create_info)) {
-    base::Log::Info("renderer", "pipeline layout", "created");
+PipelineLayout::PipelineLayout(const VkDevice &device,
+                               const VkPipelineLayoutCreateInfo &create_info)
+    : device_(device), pipeline_layout_(Create(create_info)) {
+  base::Log::Info("renderer", "pipeline layout", "created");
 }
 
 PipelineLayout::~PipelineLayout() {
-    base::Log::Info("renderer", "pipeline layout", "destroying..");
-    vkDestroyPipelineLayout(device_, pipeline_layout_, nullptr);
+  base::Log::Info("renderer", "pipeline layout", "destroying..");
+  vkDestroyPipelineLayout(device_, pipeline_layout_, nullptr);
 }
 
-const VkPipelineLayout& PipelineLayout::Get() const {
-    return pipeline_layout_;
+const VkPipelineLayout &PipelineLayout::Get() const { return pipeline_layout_; }
+
+VkPipelineLayout
+PipelineLayout::Create(const VkPipelineLayoutCreateInfo &create_info) const {
+  VkPipelineLayout pipeline_layout;
+  ErrorCheck(
+      vkCreatePipelineLayout(device_, &create_info, nullptr, &pipeline_layout));
+  return pipeline_layout;
 }
 
-VkPipelineLayout PipelineLayout::Create(const VkPipelineLayoutCreateInfo &create_info) const {
-    VkPipelineLayout pipeline_layout;
-    ErrorCheck(vkCreatePipelineLayout(device_, &create_info, nullptr, &pipeline_layout));
-    return pipeline_layout;
-}
-
-}
+} // namespace lsim::renderer::vlk
