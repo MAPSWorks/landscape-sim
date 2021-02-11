650417db14bdf0cf4018f55fe0cb8ad28626b703
ivarsrb <ivars.rb@gmail.com>
2021-02-11 14:44:20 +0200

    Add command pool module

---
+++ include/lsim/renderer/vlk/command_pool.h
@@ -0,0 +1,15 @@
+//
+// Created by Ivars Rusbergs in 2021
+//
+// Abstraction of a Vulkan command pool object.
+#ifndef LSIM_RENDERER_VLK_COMMAND_POOL_H_
+#define LSIM_RENDERER_VLK_COMMAND_POOL_H_
+#include <vulkan/vulkan.h>
+
+namespace lsim::renderer::vlk {
+
+
+}
+
+#endif
+
---
+++ src/renderer/vlk/command_pool.cc
@@ -0,0 +1,5 @@
+
+//
+// Created by Ivars Rusbergs in 2021
+//
+#include "lsim/renderer/vlk/command_pool.h"
