4aac9c6ce8b09e7ca0242cfd7f5052a2cc503c66
ivarsrb <ivars.rb@gmail.com>
2021-02-11 10:58:15 +0200

    Add framebuffer module

---
+++ include/lsim/renderer/vlk/framebuffer.h
@@ -0,0 +1,18 @@
+//
+// Created by Ivars Rusbergs in 2021
+//
+// Abstraction of a Vulkan framebuffer layout object.
+#ifndef LSIM_RENDERER_VLK_FRAMEBUFFER_H_
+#define LSIM_RENDERER_VLK_FRAMEBUFFER_H_
+#include <vulkan/vulkan.h>
+
+
+
+namespace lsim::renderer::vlk {
+class Framebuffer {
+public:
+private:
+
+  };
+}
+  #endif
---
+++ src/renderer/vlk/framebuffer.cc
@@ -0,0 +1,9 @@
+//
+// Created by Ivars Rusbergs in 2021
+//
+#include "lsim/renderer/vlk/framebuffer.h"
+
+namespace lsim::renderer::vlk {
+
+
+  }
