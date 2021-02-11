36b002e7ea4e5b57915f29d31c113d0c98212d32
ivarsrb <ivars.rb@gmail.com>
2021-02-11 14:56:48 +0200

    Implement command pool

--- include/lsim/renderer/vlk/command_pool.h
+++ include/lsim/renderer/vlk/command_pool.h
@@ -6,10 +6,28 @@
 #define LSIM_RENDERER_VLK_COMMAND_POOL_H_
 #include <vulkan/vulkan.h>
 
+#include "device_queue.h"
+
 namespace lsim::renderer::vlk {
+class CommandPool {
+public:
+  // is_transient - true for short-lived buffers for optimization
+  CommandPool(const VkDevice &device, QueueFamilyIndex family_index,
+              bool is_resetable, bool is_transient = false);
+  ~CommandPool();
+  CommandPool(CommandPool const &) = delete;
+  CommandPool operator=(CommandPool const &) = delete;
+  const VkCommandPool &Get() const;
+  //VkCommandBuffer AllocateCommandPrimaryBuffer() const;
 
+private:
+  VkCommandPool Create(QueueFamilyIndex family_index, bool is_resetable,
+                       bool is_transient) const;
+  // Reference to resource this object is created with
+  const VkDevice &device_;
+  const VkCommandPool command_pool_ = VK_NULL_HANDLE;
+};
 
-}
+} // namespace lsim::renderer::vlk
 
 #endif
-
--- src/renderer/vlk/command_pool.cc
+++ src/renderer/vlk/command_pool.cc
@@ -1,5 +1,64 @@
-
 //
 // Created by Ivars Rusbergs in 2021
 //
 #include "lsim/renderer/vlk/command_pool.h"
+
+#include "lsim/base/log.h"
+#include "lsim/renderer/vlk/device_queue.h"
+#include "vulkan_shared.h"
+
+namespace lsim::renderer::vlk {
+CommandPool::CommandPool(const VkDevice &device, QueueFamilyIndex family_index,
+                         bool is_resetable, bool is_transient)
+    : device_(device),
+      command_pool_(Create(family_index, is_resetable, is_transient)) {
+  base::Log::Info("renderer", "command pool", "created");
+}
+
+// Command buffers are destroyed together with pool
+CommandPool::~CommandPool() {
+  base::Log::Info("renderer", "command pool (and implicitly command buffers)",
+                  "destroying..");
+  vkDestroyCommandPool(device_, command_pool_, nullptr);
+}
+
+const VkCommandPool &CommandPool::Get() const { return command_pool_; }
+
+/*
+// Command buffer alloceted will be destroyed implicitly with pool destruction
+VkCommandBuffer CommandPool::AllocateCommandPrimaryBuffer() const {
+  VkCommandBufferAllocateInfo command_buffer_allocinfo{};
+  command_buffer_allocinfo.sType =
+      VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+  command_buffer_allocinfo.commandPool = command_pool_;
+  command_buffer_allocinfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+  command_buffer_allocinfo.commandBufferCount = 1;
+  VkCommandBuffer command_buffer;
+  ErrorCheck(vkAllocateCommandBuffers(device_, &command_buffer_allocinfo,
+                                      &command_buffer));
+  return command_buffer;
+}
+*/
+
+VkCommandPool CommandPool::Create(QueueFamilyIndex family_index,
+                                  bool is_resetable, bool is_transient) const {
+  VkCommandPoolCreateInfo pool_create_info{};
+  pool_create_info.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+  pool_create_info.queueFamilyIndex = family_index;
+  pool_create_info.flags = 0;
+  // Command buffer can be re-recorded
+  if (is_resetable) {
+    pool_create_info.flags |= VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+  }
+  // for short-lived buffers
+  if (is_transient) {
+    pool_create_info.flags |= VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
+  }
+  base::Log::Info("renderer", "command pool", "queue family index set to",
+                  family_index);
+  VkCommandPool command_pool;
+  ErrorCheck(
+      vkCreateCommandPool(device_, &pool_create_info, nullptr, &command_pool));
+  return command_pool;
+}
+} // namespace lsim::renderer::vlk
